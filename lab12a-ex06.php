738c5efd0f214fe4856aa42cee5a9195f0683987
Ryan J. Wong <rwong210@mtroyal.ca>
2020-11-08 15:58:49 -0700

    finihsed ex06

--- lab12a-ex06.php
+++ lab12a-ex06.php
@@ -1,23 +1,30 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <meta charset="utf-8"/>  
-    <title>Lab 12a</title>   
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet">    
-    <link rel="stylesheet" href="css/lab12a-ex06.css">
+  <meta charset="utf-8" />
+  <title>Lab 12a</title>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet">
+  <link rel="stylesheet" href="css/lab12a-ex06.css">
 </head>
+
 <body>
-<main class="container">
+  <main class="container">
     <article class="box">
       <div class="pagination">
-        <a href="#">1</a>
-        <a href="#" class="active">2</a>
-        <a href="#">3</a>
-        <a href="#">4</a>
-        <a href="#">5</a>
-        <a href="#">6</a>
+        <?php
+        $start = 10;
+        $end = 21;
+        $active = 16;
+        for ($i = $start; $i < $end; $i++) {
+          echo "<a href='#' ";
+          if ($i == $active) echo "class='active'";
+          echo ">$i</a>";
+        }
+        ?>
       </div>
-    </article>      
-</main>    
+    </article>
+  </main>
 </body>
+
 </html>
