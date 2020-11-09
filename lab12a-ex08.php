332fce714e869da62f917a9c89c112f28c657987
Ryan J. Wong <rwong210@mtroyal.ca>
2020-11-08 17:51:05 -0700

    ex08 finished

--- lab12a-ex08.php
+++ lab12a-ex08.php
@@ -6,8 +6,20 @@
 </head>
 <body>
 <?php
-   // add code here
-   
-?>  
+   // coded added here
+   $outside = "I'm outside of any functions";
+    echo "Outside=" . $outside . "<br/>";
+
+    function testScope($param) {
+        $inside = "I'm inside a function";
+        echo "Inside=" . $inside . "<br/>";
+        echo "Param=" . $param . "<br/>";
+        //echo "Outside=" . $outside . "<br/>";
+}
+
+   // now invoke that 
+   // testScope("I'm a parameter");
+   testScope($outside);
+?> 
 </body>
 </html>
